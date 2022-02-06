ae740dafb1a936ef4b6157645ff13665a2f0407b
Juan Martin Cechetto <juan.cechetto@bigbox.com.ar>
2022-02-06 18:05:29 -0300

    add autocannon, 0x still not working

--- routes/randomRoute.js
+++ routes/randomRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { fork } from "child_process";
+// import { fork } from "child_process";
 import logger from "../helpers/pino.js";
 
 const { Router } = express;
@@ -8,14 +8,14 @@ const router = new Router();
 
 router.get('/', async (req, res) => {
   logger.info("GET /api/randoms")
-  let calc = fork("./helpers/randoms.js", [req.query?.cant]);
-  calc.on("message", (msg) => { 
+  // let calc = fork("./helpers/randoms.js", [req.query?.cant]);
+  // calc.on("message", (msg) => { 
     if (msg === "READY") {
       calc.send("START");
     } else {
       res.json({ msg });
     }
-  });
+  // });
 });
 
 export default router;
--- uncommit/docker.md
+++
@@ -1,3 +0,0 @@
-# En la carpeta ~/my-docker/
-
-sudo docker run -i -t -p "80:80" -p "3306:3306" -v ${PWD}/app:/app -v ${PWD}/mysql:/var/lib/mysql 2d9935d1db98
